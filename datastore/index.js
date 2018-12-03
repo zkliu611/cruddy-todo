564c087b719ae9e2dec7c51e101ada26ebf6972d
zkliu611 <zkliu61@gmail.com>
2018-12-03 15:52:39 -0800

    Implement readOne

--- datastore/index.js
+++ datastore/index.js
@@ -37,11 +37,12 @@ exports.readAll = (callback) => {
 
 exports.readOne = (id, callback) => {
   var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
+  fs.readFile(path.join(exports.dataDir, id + '.txt'), 'utf8', (err, text) => {
+    if (err) {
+      return callback(new Error(`No item with id: ${id}`));
+    }
     callback(null, { id, text });
-  }
+  });
 };
 
 exports.update = (id, text, callback) => {
